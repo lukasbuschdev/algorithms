b10afc94d30f2bfff7ff9914d82109c59f6304c3
lukasbuschdev <buschlukas175@gmail.com>
2025-05-20 23:58:58 -0600

    implemented more functions templates and selectionSort() algorithm

--- sort.js
+++ sort.js
@@ -1,11 +1,14 @@
-const arr = Array.from({ length: 100000 }, (_, i) => i + 1);
+const arr = Array.from({ length: 100 }, (_, i) => i + 1);
 arr.sort(() => Math.random() - 0.5);
 
 
 init();
 
 function init() {
-    standardSort();
+    // standardSort();
+    selectionSort();
+    // bubbleSort();
+    // insertionSort();
 }
 
 
@@ -28,7 +31,21 @@ function standardSort() {
 function selectionSort() {
     const start = performance.now();
     
-    // CODE
+    for(let i = 0; i < arr.length; i++) {           // outer loop: iterating over each position of arr
+        let minIndex = i;                           // assume: smallest number is a index i 
+
+        for(let j = i + 1; j < arr.length; j++) {   // inner loop: looking for smallest number; starting at index 'i + 1' -> everything before is already sorted
+            if(arr[j] < arr[minIndex]) {            // check if: number at current index 'arr[j]' is smaller than number at index arr[minIndex]
+                minIndex = j;                       // if true: set the index 'minIndex' to the current index 'j'
+            }
+
+            if(minIndex !== i) {                    // check if: index 'minIndex' is not the same as the index 'i' of the current outer loop
+                let temp = arr[i];                  // save: current number 'arr[i]' in a temporary variable
+                arr[i] = arr[minIndex];             // set: number 'arr[i]' to number 'arr[minIndex]'
+                arr[minIndex] = temp;               // set: number 'arr[minIndex]' to temporary saved number 'temp' / 'arr[i]' 
+            }
+        }
+    }
 
     const end = performance.now();
     console.log('Elapsed time: ', (end - start).toFixed(2))
